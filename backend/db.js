591b505cee78171a4ae6f66a72152e060973cd85
Samuel <83781440+samuelmarc@users.noreply.github.com>
2026-04-22 18:20:57 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_USER || 'gamer_registry',
+    database: process.env.DB_NAME || 'gamer_registry',
     password: process.env.DB_PASSWORD || 'root',
     port: process.env.DB_USER || 5432,
 });
