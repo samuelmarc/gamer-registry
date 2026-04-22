f88b553457971cac5a39250291cd6c566a570302
Samuel <83781440+samuelmarc@users.noreply.github.com>
2026-04-22 18:22:45 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-/* ===== ROTAS DA API ===== */
 const gamersRoutes = require('./routes/gamers.routes');
 const gamesRoutes = require('./routes/games.routes');
 const gamerGamesRoutes = require('./routes/gamerGames.routes');
@@ -14,17 +13,16 @@ app.use('/api/gamers', gamersRoutes);
 app.use('/api/games', gamesRoutes);
 app.use('/api/gamer-games', gamerGamesRoutes);
 
-
-/* ===== FRONTEND ===== */
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-
-/* ===== ROTA PADRÃO ===== */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
+if (require.main === module) {
+    app.listen(3000, () => {
+        console.log('Servidor rodando em http://localhost:3000');
+    });
+}
 
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
-});
+module.exports = app;
